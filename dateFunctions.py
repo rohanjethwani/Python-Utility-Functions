c416e0f77df7034162f57ba031215b3ad5fd4283
Rohan Jethwani <83866548+rohanjethwani@users.noreply.github.com>
2022-04-04 19:46:58 +0530

    Update dateFunctions.py

--- dateFunctions.py
+++ dateFunctions.py
@@ -2,43 +2,43 @@ import datetime
 import time
 
 def getDateFromString(dateTime, inputFormat, outputFormat):
-  """
-  This function parses date/time using inputFormat and returns date/time in outputFormat
-  :param dateTime:
-  :param inputFormat:
-  :param outputFormat:
-  :return: formattedDateTime
-  """
-  parsedDateTime = datetime.datetime.strptime(dateTime, inputFormat)
-  formattedDateTime = datetime.datetime.strftime(parsedDateTime, outputFormat)
+    """
+    This function parses date/time using inputFormat and returns date/time in outputFormat
+    :param dateTime:
+    :param inputFormat:
+    :param outputFormat:
+    :return: formattedDateTime
+    """
+    parsedDateTime = datetime.datetime.strptime(dateTime, inputFormat)
+    formattedDateTime = datetime.datetime.strftime(parsedDateTime, outputFormat)
   
-  return formattedDateTime
+    return formattedDateTime
 
 
 def getCurrentDateTime():
-  """
-  This function returns current date time (E.g. 2021-07-13 17:30:00.000000)
-  """
-  return datetime.datetime.now()
+    """
+    This function returns current date time (E.g. 2021-07-13 17:30:00.000000)
+    """
+    return datetime.datetime.now()
 
 
 def find_last_business_date(dt):
-  """
-  This function converts given date (YYYYmmdd) to last business date
-  """
-  curr_dt = datetime.datetime.strptime(dt, '%Y%m%d')
+    """
+    This function converts given date (YYYYmmdd) to last business date
+    """
+    curr_dt = datetime.datetime.strptime(dt, '%Y%m%d')
   
-  if curr_dt.weekday() == 0:  # 0=Monday
-    last_business_dt = curr_dt - datetime.timedelta(days=3)
-  elif curr_dt.weekday() == 6: # 6=Sunday
-    last_business_dt = curr_dt - datetime.timedelta(days=2)
-  else:
-    last_business_dt = curr_dt - datetime.timedelta(days=1)
+    if curr_dt.weekday() == 0:  # 0=Monday
+        last_business_dt = curr_dt - datetime.timedelta(days=3)
+    elif curr_dt.weekday() == 6: # 6=Sunday
+        last_business_dt = curr_dt - datetime.timedelta(days=2)
+    else:
+        last_business_dt = curr_dt - datetime.timedelta(days=1)
   
-  return last_business_dt.strftime('%Y%m%d')
+    return last_business_dt.strftime('%Y%m%d')
   
   
-  def concat_curr_time(from_dt, to_dt):
+def concat_curr_time(from_dt, to_dt):
     """
     This function returns the given dates with current time appended with formatting changes. E.g. from_dt = 20210713; from_dt_time = 2021-07-13 17:30:00
     """
@@ -49,7 +49,7 @@ def find_last_business_date(dt):
     return from_dt_time, to_dt_time
   
   
-  def concat_start_end_day_time(dt):
+def concat_start_end_day_time(dt):
     """
     This function returns the given date with the time range for entire day. E.g. dt = 20210713; from_dt_time = 2021-07-13 00:00:00; to_dt_time = 2021-07-13 23:59:59
     """
